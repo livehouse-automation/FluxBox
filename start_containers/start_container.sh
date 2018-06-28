6911d705b879afe77b73afb583b5972d1a0a1a9b
Mike <mike.nye@xpanse.com.au>
2018-06-29 00:52:18 +0800

    Update start_container.sh

--- start_containers/start_container.sh
+++ start_containers/start_container.sh
@@ -18,14 +18,18 @@ docker run \
   -d \
   --rm \
   --name influxdb \
+  --network livehouse \
   -v /storage/influxdb/data:/var/lib/influxdb \
   influxdb:latest
 
 # recreate grafana container
+mkdir -p /storage/grafana/data
+chown -R 472:472 /storage/grafana
 docker run \
   -d \
   --rm \
   --name grafana \
+  --network livehouse \
   -p 3000:3000 \
   -v /storage/grafana/data:/var/lib/grafana \
   livehouseautomation/veraflux-grafana:latest
