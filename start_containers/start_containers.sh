0adcafe035ecbd0b97ba16d5080eb26128badf92
mikenye <mike.nye@xpanse.com.au>
2018-06-30 13:19:44 +0800

    new file:   start_containers.sh 	deleted:    start_container.sh

---
+++ start_containers/start_containers.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# create network
+docker network create livehouse
+
+# pull latest container images
+docker pull influxdb:latest
+docker pull telegraf:latest
+docker pull livehouseautomation/veraflux-grafana:latest
+
+# stop running containers
+docker container rename influxdb influxdb_old
+docker container rename telegraf telegraf_old
+docker container rename grafana grafana_old
+docker stop influxdb
+docker stop telegraf
+docker stop grafana
+
+# recreate influxdb container
+docker run \
+  -d \
+  --rm \
+  --name influxdb \
+  --network livehouse \
+  -v /storage/influxdb/data:/var/lib/influxdb \
+  influxdb:latest
+
+# recreate grafana container
+mkdir -p /storage/grafana/data
+chown -R 472:472 /storage/grafana
+docker run \
+  -d \
+  --rm \
+  --name grafana \
+  --network livehouse \
+  -p 3000:3000 \
+  -v /storage/grafana/data:/var/lib/grafana \
+  livehouseautomation/veraflux-grafana:latest
+
