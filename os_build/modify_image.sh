c1d2492534892c6afd1c5f0ce5cb386e8bb11b54
Mike Nye <mike.nye@xpanse.com.au>
2018-07-02 20:50:02 +0800

    updates

--- os_build/modify_image.sh
+++ os_build/modify_image.sh
@@ -7,53 +7,70 @@ git clone -b develop https://github.com/livehouse-automation/FluxBox.git
 # TODO ^^^^ remove develop branch when ready
 
 # alias for running commands in the image
-alias run_in_image='proot -q qemu-arm -r /imageroot'
+alias run_in_image='proot -q qemu-arm -0 -r /imageroot'
+
+
+#### CODE BELOW SHOULD BE RUN IN IMAGE
+
 
 # Sort out dns in container for this process
-echo nameserver 8.8.8.8 > /imageroot/etc/resolv.conf
-echo nameserver 8.8.4.4 >> /imageroot/etc/resolv.conf
+echo nameserver 8.8.8.8 > /etc/resolv.conf
+echo nameserver 8.8.4.4 >> /etc/resolv.conf
 
+# fix issue with apt
+#mv -v /imageroot/etc/passwd /imageroot/etc/passwd.original
+#grep -v "^_apt" /imageroot/etc/passwd.original > /imageroot/etc/passwd
 
 # refresh apt inside image
-run_in_image apt-get update -y
+apt-get update -y
 
 # upgrade anything that needs it
-run_in_image apt-get upgrade -y
+apt-get upgrade -y
 
 # various prerequisites
-run_in_image apt-get install -y curl net-tools
+apt-get install -y curl net-tools
 
 # Hard Drive Disk Parking Safely
 # https://wiki.odroid.com/odroid-xu4/troubleshooting/shutdown_script
-run_in_image apt-get install -y hdparm
-run_in_image curl -fsSL https://dn.odroid.com/5422/script/odroid.shutdown -o /lib/systemd/system-shutdown/odroid.shutdown
-run_in_image chown root:root /lib/systemd/system-shutdown/odroid.shutdown
-run_in_image chmod 0755 /lib/systemd/system-shutdown/odroid.shutdown
+apt-get install -y hdparm
+curl -fsSL https://dn.odroid.com/5422/script/odroid.shutdown -o /lib/systemd/system-shutdown/odroid.shutdown
+chown root:root /lib/systemd/system-shutdown/odroid.shutdown
+chmod 0755 /lib/systemd/system-shutdown/odroid.shutdown
 
 # Install NTP
-run_in_image apt-get install -y ntp
+apt-get install -y ntp
 
 # Install docker
-run_in_image apt-get install -y dmsetup
-run_in_image curl -fsSL get.docker.com | bash
-run_in_image docker run --rm hello-world
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce
 
 # Create livehouse network
-run_in_image docker network create livehouse
+docker network create livehouse
 
 # Pull livehouse images
-run_in_image docker pull influxdb:latest
-run_in_image docker pull livehouseautomation/veraflux-grafana:latest
+docker pull influxdb:latest
+docker pull livehouseautomation/veraflux-grafana:latest
 
 # Install telegraf for monitoring underlying system
-run_in_image curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-run_in_image source /etc/lsb-release
-run_in_image echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-run_in_image apt-get update -y
-run_in_image apt-get install -y telegraf
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+apt-get update -y
+apt-get install -y telegraf
 
 # Copy scripts
-run_in_image mkdir -p /opt/livehouse/
+mkdir -p /opt/livehouse/
 cp -v /src/FluxBox/start_containers/start_containers.sh /imageroot/opt/livehouse/
 cp -v /src/FluxBox/startup_config/startup_config.py /imageroot/opt/livehouse/
 cp -v /src/FluxBox/startup_config/config.ini /imageroot/media/boot/
