3ab5b13a084411dab65ff8de4ef8c1d451e22d7e
Mike Nye <mike.nye@xpanse.com.au>
2018-07-06 17:08:56 +0800

    updates

--- os_build/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build -t fluxbox-os-build .
-docker run \
-    --privileged \
-	--name fluxbox-os-build \
-	--rm \
-	-it \
-	-v /home/mikenye/odroid/root:/imageroot \
-	fluxbox-os-build
